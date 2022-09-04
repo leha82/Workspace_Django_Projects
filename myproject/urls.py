7837ff20b09f067bb19e6aef5d0893be2f06e291
leha82 <leha82@kunsan.ac.kr>
2022-09-04 14:39:21 +0900

    create first views

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# http://127.0.0.1/
+# http://127.0.0.1/app/
+
+# http://127.0.0.1/create/
+# http://127.0.0.1/read/1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('myapp.urls'))
 ]
